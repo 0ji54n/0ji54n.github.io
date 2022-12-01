f05e8ea73101922c53a2affd450832fc5ceb6297
nghia le <lexuannghia143@gmail.com>
2022-12-01 07:49:41 +0700

    hack font fixing

--- src/components/background.js
+++ src/components/background.js
@@ -25,7 +25,7 @@ const theme = extendTheme({
   components,
   styles,
   fonts:{
-    body: "Hack",
+    body: `'Hack', sans-serif`,
   }
 });
 
--- src/components/font.js
+++ src/components/font.js
@@ -3,7 +3,20 @@ import React from 'react';
 const Fonts = () => (
     <Global
       styles={`
-      @font-face {font-family: "Calibri"; src: url("//db.onlinewebfonts.com/t/a78cfad3beb089a6ce86d4e280fa270b.eot"); src: url("//db.onlinewebfonts.com/t/a78cfad3beb089a6ce86d4e280fa270b.eot?#iefix") format("embedded-opentype"), url("//db.onlinewebfonts.com/t/a78cfad3beb089a6ce86d4e280fa270b.woff2") format("woff2"), url("//db.onlinewebfonts.com/t/a78cfad3beb089a6ce86d4e280fa270b.woff") format("woff"), url("//db.onlinewebfonts.com/t/a78cfad3beb089a6ce86d4e280fa270b.ttf") format("truetype"), url("//db.onlinewebfonts.com/t/a78cfad3beb089a6ce86d4e280fa270b.svg#Calibri") format("svg"); }
+      @font-face{
+        font-family:anurati;
+        font-style:normal;
+        font-weight:400;src:local('Anurati');
+        src: url(../fonts/Anurati-Regular.woff) format('woff');
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+      }
+      @font-face {
+        font-family: 'Calibri';
+        font-style: normal;
+        font-weight: 400;
+        src: url(https://fonts.gstatic.com/l/font?kit=J7afnpV-BGlaFfdAhLEY6w&skey=a1029226f80653a8&v=v15) format('woff2');
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+      }
         `}
     />
   )
